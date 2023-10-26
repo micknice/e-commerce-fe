f5ff3827b9a1255d29ed3f4b3a7f55f6f2fd8019
mick nice <mikemccullagh@outlook.com>
2023-10-26 13:22:04 +0100

    adds register and forgot password components

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -3,30 +3,54 @@ import RecaptchaImg from '../../public/assets/RecaptchaLogo.svg.png'
 
 
 
-const Login = () => {
+const Register = () => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 px-4">
             <div className="flex flex-col">
                 <div className="h-20 flex justify-center items-center">
-                    <p className="text-2xl text-mira-black font-bold tracking-tighter">CUSTOMER LOGIN</p>
+                    <p className="text-xl text-mira-black font-bold tracking-tighter">CREATE NEW CUSTOMER ACCOUNT</p>
                 </div>
-                <p className="text-xl text-mira-black font-semibold tracking-widest">Registered Customers</p>
+                <p className="text-xl text-mira-black font-semibold tracking-widest">Personal Information</p>
                 <div className="h-3"/>
                 <div className="h-[1px] bg-gray-200"/>
                 <form className="flex flex-col gap-y-3">
-                    <p className="text-sm pt-4">Enter your email and password to login.</p>
                     <div className="pt-2">
                         <div className="flex gap-x-2">
-                            <p className="text-sm">Email</p><p className="text-mira-text-red">*</p>
+                            <p className="text-sm font-semibold">First Name</p><p className="text-mira-text-red">*</p>
                         </div>
                         <input type="text" className="h-9 w-3/4 border-gray-200"></input>
                     </div>
                     <div>
                         <div className="flex gap-x-2">
-                            <p className="text-sm">Password</p><p className="text-mira-text-red">*</p>
+                            <p className="text-sm font-semibold">Last Name</p><p className="text-mira-text-red">*</p>
                         </div>
                         <input type="text" className="h-9 w-3/4 border-gray-200"></input>
                     </div>
+                    <div className="flex flex-row gap-x-2 items-center">
+                        <input type="checkbox" className="h-3 w-3"></input>
+                        <p className="text-sm">Allow remote shopping assistance.</p>
+                    </div>
+                    <div className="h-3"/>
+
+                    <p className="text-xl text-mira-black font-semibold tracking-widest">Sign-in Information</p>
+                    <div className="h-[1px] bg-gray-200"/>
+                    <div className="pt-2">
+                        <div className="flex gap-x-2">
+                            <p className="text-sm font-semibold">Email</p><p className="text-mira-text-red">*</p>
+                        </div>
+                        <input type="text" className="h-9 w-3/4 border-gray-200"></input>
+                    </div>
+                    <div>
+                        <div className="flex gap-x-2">
+                            <p className="text-sm font-semibold">Password</p><p className="text-mira-text-red">*</p>
+                        </div>
+                        <input type="text" className="h-9 w-3/4 border-gray-200"></input>
+                    </div>
+                    <div className="flex flex-row gap-x-2 items-center">
+                        <input type="checkbox" className="h-3 w-3"></input>
+                        <p className="text-sm">Show Password.</p>
+                    </div>
+                    <div className='h-2'/>
                     <div className="flex flex-row gap-x-2 items-center">
                         <input type="checkbox" className="h-3 w-3"></input>
                         <p className="text-sm">Remember Me.</p>
@@ -44,30 +68,14 @@ const Login = () => {
                     </div>
                     <div className=''/>
                     <div className='bg-mira-orange h-11 w-1/2 flex justify-center items-center'>
-                        <p className='text-white text-xs font-medium'>SIGN IN</p>
+                        <p className='text-white text-xs font-medium'>SIGN UP</p>
                     </div>
-                    <p className="text-sm pt-3">Forgot Your Password?</p>
-                    <div className='flex items-center justify-center pt-2'>
-                        <p className="text-mira-text-red text-xs">* Required Fields</p>
-                    </div>
+                    <div className='h-5'/>                    
                 </form>
                 
                
             </div>
-            <div className='pt-9'>
-                <p className="text-xl text-mira-black font-semibold tracking-widest">New Customers</p>
-                <div className="h-3"/>
-                <div className="h-[1px] bg-gray-200"/>
-                <p className="text-sm pt-4 ">Creating an account has many benefits: check out faster,
-                keep more than one address, track orders and more.
-                </p>
-                <div className='h-4'/>
-                <div className='bg-mira-orange h-11 w-1/2 flex justify-center items-center'>
-                        <p className='text-white text-xs font-medium'>SIGN UP</p>
-                </div>
-                <div className='h-6'/>
-                
-            </div>
+            
 
         </div>
     )
@@ -77,4 +85,4 @@ const Login = () => {
 }
 
 
-export default Login
+export default Register
