c0e8ae3e896872d999c45393e88626ff94e3a47a
mick nice <mikemccullagh@outlook.com>
2023-12-08 06:03:21 +0000

    changes for deployed back end

--- url.js
+++ url.js
@@ -1,3 +1,3 @@
 // export const activeUrl = 'https://mikifit-be2.onrender.com'
-export const activeUrl = 'https://54.78.250.60'
+export const activeUrl = 'http://54.78.250.60'
 // export const activeUrl = 'http://localhost:8080'
